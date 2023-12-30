b5b46e4376ca0e768b2ca55e953e686befa7056a
adam.arkuszynski <adam.arkuszynski@theheart.dev>
2023-12-30 10:17:25 +0100

    update config

--- .config/LazyVim/lua/config/keymaps.lua
+++ .config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local discipline = require("custom-functions.discipline")
+
+discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- navigate within insert mode
+keymap.set("i", "<C-h>", "<Left>")
+keymap.set("i", "<C-l>", "<Right>")
+keymap.set("i", "<C-j>", "<Down>")
+keymap.set("i", "<C-k>", "<Up>")
